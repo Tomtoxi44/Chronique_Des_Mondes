db3d3c6e195967c93a09a2eec5b7aeea8a69d155
Tommy <tangibaud@3bstudio.fr>
2025-10-17 14:07:21 +0200

    feat(database): Add EF Core migration for User.Nickname column
    - Migration: 20251017120648_AddNicknameToUser
    - SQL: ALTER TABLE Users ADD Nickname nvarchar(50) NOT NULL
    - Type: nvarchar(50) with [Required] constraint
    - Default value: Empty string
    
    This migration synchronizes the database schema with the User model changes.

--- Cdm/Cdm.Migrations/Migrations/20251017120648_AddNicknameToUser.Designer.cs
+++ Cdm/Cdm.Migrations/Migrations/20251017120648_AddNicknameToUser.Designer.cs
@@ -4,6 +4,7 @@ using Cdm.Migrations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cdm.Migrations.Migrations
 {
     [DbContext(typeof(MigrationsContext))]
-    partial class MigrationsContextModelSnapshot : ModelSnapshot
+    [Migration("20251017120648_AddNicknameToUser")]
+    partial class AddNicknameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
